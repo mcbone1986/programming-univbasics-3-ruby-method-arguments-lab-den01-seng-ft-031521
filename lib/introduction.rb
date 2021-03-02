e2317b2544b62ce5ac4138979042a88811800b33
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 00:18:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,11 +16,11 @@ end
 
 introduction_with_language
 
-def introduction_with_language_optional(name = "Matt", language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Ruby")
+introduction_with_language_optional("Matt")
 
 
 
